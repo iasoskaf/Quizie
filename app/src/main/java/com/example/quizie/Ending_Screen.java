7e7dea77cfb7b0b859a60595394a221b0c4f0d15
chrizygo <xristina.zug@gmail.com>
2021-02-21 22:13:30 +0200

    .

--- app/src/main/java/com/example/quizie/Ending_Screen.java
+++ app/src/main/java/com/example/quizie/Ending_Screen.java
@@ -1,8 +1,18 @@
 package com.example.quizie;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,18 +24,43 @@ public class Ending_Screen extends AppCompatActivity {
    // TextView mistakes_result;
   //  public Intent mIntent = getIntent();
    // int intValue = mIntent.getIntExtra("intVariableName", 0);
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+       //creating chanel
+        NotificationChannel channel = new NotificationChannel("Notification", "Notification",NotificationManager.IMPORTANCE_DEFAULT);
+        NotificationManager manager = getSystemService(NotificationManager.class);
+        manager.createNotificationChannel(channel);
+
         //String final_mistakes = Integer.toString(intValue);
         //mistakes_result = findViewById(R.id.mistakes_result);
        // mistakes_result.setText(final_mistakes);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ending__screen);
-     returnButton = findViewById(R.id.return_button);
+     returnButton =(Button) findViewById(R.id.return_button);
      returnButton.setOnClickListener(new View.OnClickListener() {
          @Override
          public void onClick(View v) {
+             //notification code
+             Intent intent = new Intent(getApplicationContext(),Work_It_Quiz.class);
+             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+             PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(),1,intent,PendingIntent.FLAG_ONE_SHOT);
+
+
+             NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(),"Notification");
+             builder.setContentTitle("Notification");
+             builder.setContentText("Play another quiz!!");
+             builder.setSmallIcon(R.drawable.ic_message);
+             builder.setContentIntent(pendingIntent);
+             builder.setAutoCancel(true);
+
+             NotificationManagerCompat managerCompat = NotificationManagerCompat.from(Ending_Screen.this);
+             managerCompat.notify(1,builder.build());
+
+
              openActivity_Movies();
+
          }
      });
 
@@ -34,4 +69,5 @@ public class Ending_Screen extends AppCompatActivity {
         Intent intent= new Intent(this, MoviesActivity.class);
         startActivity(intent);
     }
+
 }
