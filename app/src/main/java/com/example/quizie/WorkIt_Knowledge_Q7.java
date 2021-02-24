09578b479ce6be8e4a5ab4ccc21e059b03a6b637
chrizygo <xristina.zug@gmail.com>
2021-02-24 17:46:24 +0200

    .

--- app/src/main/java/com/example/quizie/WorkIt_Knowledge_Q7.java
+++ app/src/main/java/com/example/quizie/WorkIt_Knowledge_Q7.java
@@ -1,5 +1,6 @@
 package com.example.quizie;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -13,6 +14,9 @@ public class WorkIt_Knowledge_Q7 extends AppCompatActivity {
     public Button a,b,c,complete;
     //public Intent mIntent = getIntent();
     //int intValue = mIntent.getIntExtra("intVariableName", 0);
+    private AlertDialog.Builder dialogbuilder;
+    private AlertDialog dialog;
+    private Button close_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,7 +48,6 @@ public class WorkIt_Knowledge_Q7 extends AppCompatActivity {
         c.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 c.setTextColor(Color.GREEN);
                 Toast.makeText(getApplicationContext(),"Correct", Toast.LENGTH_SHORT).show();
                 complete.setEnabled(true);
@@ -55,11 +58,33 @@ public class WorkIt_Knowledge_Q7 extends AppCompatActivity {
         complete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //createNewContactDialog();
                 openActivity_Ending_Screen();
             }
         });
 
     }
+    public void createNewContactDialog(){
+        dialogbuilder = new AlertDialog.Builder(this);
+        final View contactPopupView = getLayoutInflater().inflate(R.layout.pop_up,null);
+        close_btn = contactPopupView.findViewById(R.id.close_btn);
+        dialogbuilder.setView(contactPopupView);
+        dialog= dialogbuilder.create();
+        dialog.show();
+
+       close_btn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               dialog.dismiss();
+           }
+       });
+
+
+    }
+
+
+
+
     public void openActivity_Ending_Screen(){
         Intent intent= new Intent(this, Ending_Screen.class);
         //intent.putExtra("intVariableName", intValue);
